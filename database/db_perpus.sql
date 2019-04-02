05ab83b7616380ff09e2c6825ecbf07e37b35c68
greenmr7 <greenmr791@gmail.com>
2019-04-02 21:27:18 +0700

    v2.5update database

--- database/db_perpus.sql
+++ database/db_perpus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 01 Apr 2019 pada 14.50
+-- Generation Time: 02 Apr 2019 pada 16.24
 -- Versi Server: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
 
